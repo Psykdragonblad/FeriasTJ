c988e49e06c009ff0b72247cc4893b82a06949a0
Marcos Souza <marcsantsouza@tjba.jus.br>
2024-12-05 15:13:11 -0300

    Criação inicial do model ferias

---
+++ FeriasTJ/Domain/Entities/Ferias.cs
@@ -0,0 +1,9 @@
+﻿namespace FeriasTJ.Domain.Entities
+{
+    public class Ferias
+    {
+        public int Matricula { get; set; }
+        public DateTime PeriodoAquisitivoInicial { get; set; }
+        public DateTime PeriodoAquisitivoFinal { get; set; }
+    }
+}

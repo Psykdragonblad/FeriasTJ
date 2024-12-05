4f4201d4afb9d4d81fa2022fdf8a8e8c9af5e763
Marcos Souza <marcsantsouza@tjba.jus.br>
2024-12-05 12:04:06 -0300

    ajustes 3

--- FeriasTJ/Controllers/WeatherForecastController.cs
+++ FeriasTJ/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace FeriasTJ.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "New"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "New" 
         };
 
         private readonly ILogger<WeatherForecastController> _logger;

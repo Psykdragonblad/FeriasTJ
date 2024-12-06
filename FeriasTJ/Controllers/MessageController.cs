0b8dd822c51b91fe99f9fcf38b1369b7ddf59cbf
Marcos Souza <marcsantsouza@tjba.jus.br>
2024-12-06 15:56:20 -0300

    Merge

---
+++ FeriasTJ/Controllers/MessageController.cs
@@ -0,0 +1,38 @@
+﻿using FeriasTJ.Domain.Entities;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace FeriasTJ.Controllers
+{
+  
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessageController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendMessage([FromBody] Ferias message)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", Port =5672 };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var queueName = "minha-fila";
+            channel.QueueDeclare(queue: queueName,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(jsonMessage);
+            channel.BasicPublish(exchange: "",
+                                 routingKey: queueName,
+                                 basicProperties: null,
+                                 body: body);
+
+            return Ok("Mensagem enviada para a fila");
+        }
+    }
+}

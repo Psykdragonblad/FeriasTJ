320d0cf17a87f3fddc8f087ca76907b9eef22f80
Marcos Souza <marcsantsouza@tjba.jus.br>
2024-12-06 08:10:03 -0300

    Criação de objetos e das dependencias do postgres

---
+++ FeriasTJBase/Infra/Configurations/PgDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FeriasTJ.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace FeriasTJBase.Infra.Configurations
+{
+    public class PgDbContext : DbContext
+    {
+        public PgDbContext(DbContextOptions<PgDbContext> options) : base(options) { }
+
+        public DbSet<Ferias> Ferias { get; set; }
+        public DbSet<Ferias> Usufruto { get; set; }
+    }
+}
---
+++ FeriasTJBase/Infra/Repositories/Ferias.cs
@@ -0,0 +1,12 @@
+﻿namespace FeriasTJ.Domain.Entities
+{
+    public class Ferias
+    {
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public DateTime PeriodoAquisitivoInicial { get; set; }
+        public DateTime PeriodoAquisitivoFinal { get; set; }
+        public List<Usufruto> Usufrutos { get; set; }
+
+    }
+}

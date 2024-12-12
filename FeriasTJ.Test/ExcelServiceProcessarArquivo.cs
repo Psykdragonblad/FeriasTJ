badef7946b9e35d95b5a6f4654ecb3299b5b8561
carlitosilva <carlito.silva@gmail.com>
2024-12-12 14:23:49 -0300

    Criação do projeto de teste TeriasTJ.Test

---
+++ FeriasTJ.Test/ExcelServiceProcessarArquivo.cs
@@ -0,0 +1,42 @@
+using FeriasTJ.Infra.Service;
+using FeriasTJ.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Text;
+using Moq;
+using FeriasTJ.Infra.Interface;
+
+namespace FeriasTJ.Test
+{
+    public class ExcelServiceProcessarArquivo
+    {
+        private readonly IRabbitMqEnvia _rabbitMqEnvia;
+        public ExcelServiceProcessarArquivo()
+        {
+            var _rabbitMqEnvia = new Mock<IRabbitMqEnvia>();
+        }
+        [Fact]
+        public void CriarArquivoComCaminhoInvalido()
+        {
+            //arrange
+            FileUploadModel model = new FileUploadModel();
+            //model.File = new
+            var content = "";
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(content)); 
+            var formFile = new Mock<IFormFile>();
+            var fileName = "fileName.txt";
+            formFile.Setup(f => f.FileName).Returns(fileName); 
+            formFile.Setup(f => f.Length).Returns(stream.Length);
+            formFile.Setup(f => f.OpenReadStream()).Returns(stream); 
+            formFile.Setup(f => f.ContentDisposition).Returns($"attachment; filename={fileName}"); 
+            formFile.Setup(f => f.ContentType).Returns("text/plain"); 
+            formFile.Setup(f => f.CopyTo(It.IsAny<Stream>())).Callback<Stream>(s => stream.CopyTo(s));
+            model.File = formFile.Object;
+            //act
+            ExcelService service = new ExcelService(_rabbitMqEnvia);
+
+            //assert
+            Assert.Throws<IndexOutOfRangeException>(()=> service.ProcessarArquivo(model));
+        }
+    }
+}
